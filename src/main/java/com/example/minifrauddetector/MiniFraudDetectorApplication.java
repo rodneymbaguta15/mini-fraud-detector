b19569fb16409fc29e3f6f5f757c3b46d90bc523
rodney_mbaguta7 <114108814+rodneymbaguta15@users.noreply.github.com>
2026-02-20 05:00:24 +0200

    Implement fraud scoring engine and rule-based tests

--- src/main/java/com/example/minifrauddetector/MiniFraudDetectorApplication.java
+++ src/main/java/com/example/minifrauddetector/MiniFraudDetectorApplication.java
@@ -2,8 +2,10 @@ package com.example.minifrauddetector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MiniFraudDetectorApplication {
 
     public static void main(String[] args) {
