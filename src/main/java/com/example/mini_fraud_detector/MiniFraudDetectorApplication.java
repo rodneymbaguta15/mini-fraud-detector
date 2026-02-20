5b4cc1de8837c9fb2ea755b601bfc1320234204a
rodney_mbaguta7 <mbagutarodney7@gmail.com>
2026-02-19 20:17:38 -0500

    Initial spring boot scaffold

---
+++ src/main/java/com/example/mini_fraud_detector/MiniFraudDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_fraud_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFraudDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniFraudDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_fraud_detector/MiniFraudDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_fraud_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFraudDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
